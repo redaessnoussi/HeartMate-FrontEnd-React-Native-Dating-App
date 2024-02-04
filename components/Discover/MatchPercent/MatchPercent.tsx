105411773960116dc7143a9413d780f89498deca
Reda Essnoussi <71893153+redaessnoussi@users.noreply.github.com>
2024-02-04 20:53:45 +0100

    style: match rate component for card

---
+++ components/Discover/MatchPercent/MatchPercent.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+import Colors from "../../../constants/Colors";
+
+const MatchPercent = () => {
+  return (
+    <AnimatedCircularProgress
+      size={56}
+      width={6}
+      fill={70}
+      tintColor={Colors.darkPink}
+      backgroundColor={Colors.white}
+      rotation={360}
+      duration={0}
+      style={{
+        backgroundColor: Colors.glassGrey,
+        borderRadius: 28,
+      }}
+    >
+      {(fill) => (
+        <Text style={{ color: Colors.white, fontFamily: "RobotoBold" }}>
+          {fill}%
+        </Text>
+      )}
+    </AnimatedCircularProgress>
+  );
+};
+
+export default MatchPercent;
+
+const styles = StyleSheet.create({});
